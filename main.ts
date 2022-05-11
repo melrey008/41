a51527f480511b5ca348cc8927fd791a04610408
melrey008 <101406273+melrey008@users.noreply.github.com>
2022-05-11 14:27:55 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,18 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if ("1" == receivedNumber) {
+        basic.showString("a")
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(1)
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendNumber(3)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(2)
+})
+radio.setGroup(10)
+basic.forever(function () {
+	
+})
